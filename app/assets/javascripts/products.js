554b870aec0dfa0b4e64f58c17bafb3caf830bb2
Ashley <ashleykwtam@gmail.com>
2014-07-10 13:43:09 -0400

    search with $.ajax

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,5 +3,16 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).on('ready page:load', function(){
+  $('#search-form').submit(function(event){
+    event.preventDefault();
+    var searchValue = $('#search').val();
 
+    $.ajax({
+      url: '/products?search=' + searchValue,
+      type: 'GET',
+      dataType: 'html',
+    }).done(function(data){
+      $('#products').html(data);
+    });
+  });
 });
